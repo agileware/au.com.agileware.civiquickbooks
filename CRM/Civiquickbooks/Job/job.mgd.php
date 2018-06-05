e91449d3390232dfc491fea6c7c1825a61b3f952
Francis Whittle <francis@agileware.com.au>
2018-06-05 14:09:11 +1000

    Remove managed entity debugging

--- CRM/Civiquickbooks/Job/job.mgd.php
+++ CRM/Civiquickbooks/Job/job.mgd.php
@@ -3,7 +3,6 @@
 // The record will be automatically inserted, updated, or deleted from the
 // database as appropriate. For more details, see "hook_civicrm_managed" at:
 // http://wiki.civicrm.org/confluence/display/CRMDOC42/Hook+Reference
-CRM_Core_Session::setStatus('Adding Civiquickbooks entities');
 
 return array(
 	0 => array(
