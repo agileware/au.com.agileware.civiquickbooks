agileware/au.com.agileware.civiquickbooks

commit 66ee8334c69c4803cd2be167d6bafe4b7847bc42

Update for latest version of accountsync

CRM/Civiquickbooks/Invoice.php
@@ -344,7 +344,7 @@ class CRM_Civiquickbooks_Invoice {
         }
 
         $record['accounts_needs_update'] = 0;
-        $record['accounts_status_id'] = 3;
+        $record['accounts_status_id'] = 'cancelled';
 
         CRM_Core_DAO::setFieldValue(
           'CRM_Accountsync_DAO_AccountInvoice',
@@ -937,7 +937,7 @@ class CRM_Civiquickbooks_Invoice {
       'accounts_needs_update' => 1,
       'plugin' => $this->plugin,
       'connector_id' => 0,
-      'accounts_status_id' => ['NOT IN', 3],
+      'accounts_status_id' => ['NOT IN', 'completed'],
       'options' => [
         'sort' => 'error_data ASC',
         'limit' => $limit,
@@ -964,7 +964,7 @@ class CRM_Civiquickbooks_Invoice {
     $criteria = [
       'plugin' => $this->plugin,
       'connector_id' => 0,
-      'accounts_status_id' => ['NOT IN', [1, 3]],
+      'accounts_status_id' => ['NOT IN', ['completed', 'cancelled']],
       'accounts_invoice_id' => ['IS NOT NULL' => 1],
       'accounts_data' => ['IS NOT NULL' => 1],
       'error_data' => ['IS NULL' => 1],

commit b057ce6f6970ad0e99f87e7cac3a90fa5ef9bee3

CIVIQBO-107 Correct accounts status for completed invoices
This fixes a regression introduced in 66ee833

CRM/Civiquickbooks/Invoice.php
@@ -346,7 +346,7 @@ class CRM_Civiquickbooks_Invoice {
         }
 
         $record['accounts_needs_update'] = 0;
-        $record['accounts_status_id'] = 'cancelled';
+        $record['accounts_status_id'] = 'completed';
 
         CRM_Core_DAO::setFieldValue(
           'CRM_Accountsync_DAO_AccountInvoice',