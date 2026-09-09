4d1af46dbe2de9b6bc5f661f18b76e2549769b4b
agileware-justin <agileware-justin@users.noreply.github.com>
2026-09-09 05:33:51 +0000

    TASK-2296 [AUTO] civix upgraded
    Files changed:
    M	civiquickbooks.civix.php
    D	mixin/lib/civimix-schema@5.93.0/pathload.main.php
    D	mixin/lib/civimix-schema@5.93.0/src/AutomaticUpgrader.php
    D	mixin/lib/civimix-schema@5.93.0/src/CiviMixSchema.php
    D	mixin/lib/civimix-schema@5.93.0/src/DAO.php
    D	mixin/lib/civimix-schema@5.93.0/src/SchemaHelper.php
    D	mixin/lib/civimix-schema@5.93.0/src/SchemaHelperInterface.php
    D	mixin/lib/civimix-schema@5.93.0/src/SqlGenerator.php
    D	mixin/lib/pathload-0.php
    D	mixin/mgd-php@2.0.0.mixin.php
    D	mixin/smarty-v2@1.0.3.mixin.php

--- civiquickbooks.civix.php
+++ civiquickbooks.civix.php
@@ -89,8 +89,6 @@ class CRM_Civiquickbooks_ExtensionUtil {
 
 use CRM_Civiquickbooks_ExtensionUtil as E;
 
-($GLOBALS['_PathLoad'][0] ?? require __DIR__ . '/mixin/lib/pathload-0.php');
-pathload()->addSearchDir(__DIR__ . '/mixin/lib');
 spl_autoload_register('_civiquickbooks_civix_class_loader', TRUE, TRUE);
 
 function _civiquickbooks_civix_class_loader($class) {
